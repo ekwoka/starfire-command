2df1e40bde7879cc86bddbc900e579f3c21eb92b
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-10-02 00:26:39 +0800

    Update some styles

--- build/login-card.js
+++ build/login-card.js
@@ -2,7 +2,9 @@ export default function(Alpine){
     Alpine.data('loginCard',()=>({
         name: '',
         password: '',
+        loggingIn:false,
         submit(){
+            this.loggingIn=true
             Starfire.api('validate',{name:this.name,token:this.password})
         }
     }))
