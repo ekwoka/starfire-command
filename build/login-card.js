e017ca8df695b100bcb16f52e7425de0d10a53bc
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-10-08 17:53:50 +0800

    Update Discord Button

--- build/login-card.js
+++ build/login-card.js
@@ -1,8 +1,5 @@
 export default function(Alpine){
     Alpine.data('loginCard',()=>({
-        name: '',
-        password: '',
-        loggingIn:false,
         async submit(){
             this.loggingIn=true;
             window.location = `https://discord.com/api/oauth2/authorize?client_id=854194325188640819&redirect_uri=${encodeURIComponent(window.location.origin)}&response_type=code&scope=identify%20guilds`
