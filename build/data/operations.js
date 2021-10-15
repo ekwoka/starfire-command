53f0372f289dbccc15e2773c9eb6d9d4580c4f72
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-10-16 01:41:21 +0800

    initialize operation builder

--- build/data/operations.js
+++ build/data/operations.js
@@ -1,27 +1,51 @@
-import * as Starfire from '../starfire'
+import * as Starfire from '../starfire';
 
-export default function (Alpine){
-    Alpine.data('operationsSnippet',()=>({
-        operations: [],
-        totalOps: -1,
-        async init(){
-            let auth = localStorage.getItem('STARFIRE_AUTH')
-            if(!auth) return
-            let data = {
-                auth,
-                type: 'snippet'
-            }
-            let ops = await Starfire.api('operations',data)
-            if(ops.error) return console.error(ops.error)
-            this.totalOps = ops.length
-            this.operations = ops.slice(0,3)
-            this.operations.forEach(op=>op.hoursUntil = (op.time-Date.now())/3600000)
-        },
-        getTime(time,option){
-            return Starfire.time(time,option)
-        },
-        getHours(time){
-            return Math.floor((time-Date.now())/3600000)
-        }
-    }))
+export default function (Alpine) {
+  Alpine.data('operationsSnippet', () => ({
+    operations: [],
+    totalOps: -1,
+    async init() {
+      let auth = localStorage.getItem('STARFIRE_AUTH');
+      if (!auth) return;
+      let data = {
+        auth,
+        type: 'snippet'
+      };
+      let ops = await Starfire.api('operations', data);
+      if (ops.error) return console.error(ops.error);
+      this.totalOps = ops.length;
+      this.operations = ops.slice(0, 3);
+      this.operations.forEach((op) => (op.hoursUntil = (op.time - Date.now()) / 3600000));
+    },
+    getTime(time, option) {
+      return Starfire.time(time, option);
+    },
+    getHours(time) {
+      return Math.floor((time - Date.now()) / 3600000);
+    }
+  }));
+
+  Alpine.data('operationsList', () => ({
+    operations: [],
+    totalOps: -1,
+    async init() {
+      let auth = localStorage.getItem('STARFIRE_AUTH');
+      if (!auth) return;
+      let data = {
+        auth,
+        type: 'snippet'
+      };
+      let ops = await Starfire.api('operations', data);
+      if (ops.error) return console.error(ops.error);
+      this.totalOps = ops.length;
+      this.operations = ops;
+      this.operations.forEach((op) => (op.hoursUntil = (op.time - Date.now()) / 3600000));
+    },
+    getTime(time, option) {
+      return Starfire.time(time, option);
+    },
+    getHours(time) {
+      return Math.floor((time - Date.now()) / 3600000);
+    }
+  }));
 }
