5f99ec8630eacb11de2e085bb2093f677f760e11
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-10-17 19:16:38 +0800

    Add Time Parser

--- build/data/operations.js
+++ build/data/operations.js
@@ -49,24 +49,60 @@ export default function (Alpine) {
     }
   }));
 
-  Alpine.data('operationsBuilder',({} = {}) => ({
+  Alpine.data('operationsBuilder', ({} = {}) => ({
     name: '',
-    time: 0,
+    time: {
+      hour: '0000',
+      day: '',
+      get displayDate() {
+        let [time, day] = [this.hour, parseInt(this.day)];
+        let hour = parseInt(time.slice(-4, -2));
+        let minute = parseInt(time.slice(-2));
+        let date = new Date();
+
+        day = day >= 1 ? day : date.getUTCHours() > hour ? 1 : 0;
+
+        date.setUTCHours(hour?hour:0);
+        date.setUTCMinutes(minute);
+        date.setUTCSeconds(0);
+        date.setUTCDate(date.getUTCDate() + day);
+        return `${Starfire.time(date,'time')} ${Starfire.time(date,'date')}`;
+      }
+    },
     commander: '',
     location: {
       x: '',
       y: ''
     },
     description: '',
-    validateLocation(){
-      Object.entries(this.location).forEach(([key,value])=>{
-        this.location[key] = value.replace(/[^\d]+/g,'').slice(0,4)
-      })
-    },
-    init(){
-      Alpine.effect(()=>{
-        queueMicrotask(()=>this.validateLocation(this.location))
-      })
+    validate: {
+      location() {
+        Object.entries(this.location).forEach(([key, value]) => {
+          this.location[key] = value.replace(/[^\d]+/g, '').slice(0, 4);
+        });
+      },
+      hour() {
+        let time, hour, minute;
+        if (this.time.hour <= 0) return (this.time.hour = '0000');
+        time = this.time.hour.toString();
+
+        hour = time.slice(-4, -2);
+        minute = time.slice(-2);
+
+        if (minute >= 60) {
+          hour = (parseInt(hour) + 1).toString();
+          minute = '00';
+        }
+        if (parseInt(hour) >= 24) {
+          hour = '00';
+        }
+
+        this.time.hour = hour + minute;
+      },
+      day() {
+        let day = this.time.day;
+        this.time.day = day <= 0 ? 0 : day >= 14 ? 14 : day;
+      }
     }
-  }))
+  }));
 }
