033e8df24f9fc48f31caea3fb2b2ac01a32dce1a
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-10-27 03:42:45 +0800

    ➖ Removed Alpine Plugins

--- build/plugins/index.js
+++ build/plugins/index.js
@@ -1,6 +1,4 @@
-import Router from '@ekwoka/x-router'
-import Ajax from '@ekwoka/x-ajax'
 import Intersect from '@alpinejs/intersect'
 import Collapse from '@alpinejs/collapse'
 
-export default plugins = [Router, Ajax, Intersect, Collapse]
+export default plugins = [Intersect, Collapse]
